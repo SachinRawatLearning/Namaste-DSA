cc832eaa8e2d23f3ad7096b62b1e0f623ae062ba
Sachin RAWAT <sachinrawatdmc@gmail.com>
2025-07-26 19:14:10 +0530

    141 - Linked List Cycle - Floyd Algo

--- Linked List/141. Linked List Cycle.js
+++ Linked List/141. Linked List Cycle.js
@@ -50,3 +50,12 @@ var hasCycle = function (head) {
   return false;
 };
 //Space-> O(n)
+var hasCycle = function (head) {
+    let slow = fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) return true;
+    }
+    return false;
+};
