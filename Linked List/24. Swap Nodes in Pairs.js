d15e74c35046b3e8240b8a4621f87538cdc17190
Sachin RAWAT <sachinrawatdmc@gmail.com>
2025-08-10 17:31:25 +0530

    24 - Swap Nodes in Pairs

--- Linked List/24. Swap Nodes in Pairs.js
+++ Linked List/24. Swap Nodes in Pairs.js
@@ -73,3 +73,28 @@ var swapPairs = function (head) {
 
   return sentinel.next;
 };
+
+//Recursion
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+  if (!head || !head.next) return head;
+
+  let left = head;
+  let right = head.next;
+
+  left.next = swapPairs(right.next);
+  right.next = left;
+
+  return right;
+};
