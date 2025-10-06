41a13f3feb100b1e47b9225ded023d2237cc6c06
Sachin RAWAT <sachinrawatdmc@gmail.com>
2025-10-06 12:49:53 +0530

    232 Implement Queue using Stacks

--- Stacks and Queues/225. Implement Stack using Queues.js
+++ Stacks and Queues/225. Implement Stack using Queues.js
@@ -34,40 +34,120 @@
 // All the calls to pop and top are valid.
 
 // Follow-up: Can you implement the stack using only one queue?
+
 var MyStack = function () {
   this.q1 = [];
   this.q2 = [];
 };
-
+/**
+ * @param {number} x
+ * @return {void}
+ */
 MyStack.prototype.push = function (x) {
   this.q1.push(x);
 };
 
+/**
+ * @return {number}
+ */
 MyStack.prototype.pop = function () {
   let n = this.q1.length;
   for (let i = 0; i < n - 1; i++) {
-    this.q2.push(this.q1.shift());
+    let front = this.q1.shift();
+    this.q2.push(front);
   }
-  let ans = this.q1.shift();
-  let temp = this.q1;
-  this.q1 = this.q2;
-  this.q2 = temp;
-  return ans;
-};
-
-MyStack.prototype.top = function () {
-  let n = this.q1.length;
-  for (let i = 0; i < n - 1; i++) {
-    this.q2.push(this.q1.shift());
-  }
-  let front = this.q1[0];
-  this.q2.push(this.q1.shift());
+  const front = this.q1.shift();
   let temp = this.q1;
   this.q1 = this.q2;
   this.q2 = temp;
   return front;
 };
 
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function () {
+  let n = this.q1.length;
+  for (let i = 0; i < n - 1; i++) {
+    let front = this.q1.shift();
+    this.q2.push(front);
+  }
+  const front = this.q1.shift();
+  this.q2.push(front);
+  let temp = this.q1;
+  this.q1 = this.q2;
+  this.q2 = temp;
+  return front;
+};
+
+/**
+ * @return {boolean}
+ */
 MyStack.prototype.empty = function () {
   return this.q1.length === 0;
 };
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
+
+//Using One queue
+
+var MyStack = function () {
+  this.q1 = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function (x) {
+  this.q1.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function () {
+  let n = this.q1.length;
+  for (let i = 0; i < n - 1; i++) {
+    let front = this.q1.shift();
+    this.q1.push(front);
+  }
+  return this.q1.shift();
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function () {
+  let n = this.q1.length;
+  for (let i = 0; i < n - 1; i++) {
+    let front = this.q1.shift();
+    this.q1.push(front);
+  }
+  const front = this.q1.shift();
+  this.q1.push(front);
+  return front;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function () {
+  return this.q1.length === 0;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
