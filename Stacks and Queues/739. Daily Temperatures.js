11faf76b02c9c0d8e61c383e6cb1cea9305e4c9e
Sachin RAWAT <sachinrawatdmc@gmail.com>
2025-10-16 13:15:47 +0530

    739

---
+++ Stacks and Queues/739. Daily Temperatures.js
@@ -0,0 +1,46 @@
+// Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+// Example 1:
+
+// Input: temperatures = [73,74,75,71,69,72,76,73]
+// Output: [1,1,4,2,1,1,0,0]
+// Example 2:
+
+// Input: temperatures = [30,40,50,60]
+// Output: [1,1,1,0]
+// Example 3:
+
+// Input: temperatures = [30,60,90]
+// Output: [1,1,0]
+
+// Constraints:
+
+// 1 <= temperatures.length <= 105
+// 30 <= temperatures[i] <= 100
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function (temperatures) {
+  const stack = [];
+  const ans = new Array(temperatures.length).fill(0);
+  stack.push(temperatures.length - 1);
+
+  for (let i = temperatures.length - 2; i >= 0; i--) {
+    while (stack.length) {
+      if (temperatures[i] < temperatures[stack[stack.length - 1]]) {
+        const index = stack[stack.length - 1] - i;
+        ans[i] = index;
+        stack.push(i);
+        break;
+      } else {
+        stack.pop();
+      }
+    }
+    if (!stack.length) {
+      stack.push(i);
+      ans[i] = 0;
+    }
+  }
+  return ans;
+};
